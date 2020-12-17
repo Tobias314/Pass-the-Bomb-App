95bc73183e014fab94eda706fdc053ba7239b695
Tobias <pito.online@gmail.com>
2020-12-17 08:44:20 +0100

    Fix test

--- app/src/test/java/edu/bth/ma/passthebomb/client/RoundRobinSchedulerTest.kt
+++ app/src/test/java/edu/bth/ma/passthebomb/client/RoundRobinSchedulerTest.kt
@@ -13,27 +13,27 @@ import org.junit.Assert.*
 class RoundRobinSchedulerTest {
     @Test
     fun linearSchedulingTest() {
-        val scheduler = RoundRobinScheduler(3, false)
+        val scheduler = RoundRobinScheduler(3, false, {})
         for(i in 0..8){
-            assertEquals(i%3, scheduler.peekNextElement())
-            assertEquals(i%3, scheduler.peekNextElement())
-            assertEquals(i%3, scheduler.nextElement())
+            assertEquals((i+1)%3, scheduler.peekNextElement())
+            assertEquals((i+1)%3, scheduler.peekNextElement())
+            assertEquals((i+1)%3, scheduler.nextElement())
         }
     }
 
     @Test
     fun randomSchedulingTest(){
-        val scheduler = RoundRobinScheduler(3, true)
+        val scheduler = RoundRobinScheduler(3, true, {})
         val counts = ArrayList<Int>(listOf(0,0,0))
         for(i in 0..8){
+            counts[scheduler.currentElement]++
             val peeked = scheduler.peekNextElement()
             assertEquals(peeked, scheduler.peekNextElement())
             val actual = scheduler.nextElement()
             assertEquals(peeked,actual)
             if(i%3==2){
-                assertNotEquals(actual, scheduler.peekNextElement())
+                assertNotEquals(scheduler.currentElement, scheduler.peekNextElement())
             }
-            counts[actual]++
         }
         assertEquals(3, counts[0])
         assertEquals(3, counts[1])
